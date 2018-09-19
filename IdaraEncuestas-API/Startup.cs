54a9a3583c01a4790b3795a8dd3cea005e031b61
Jose Ignacio Campos Padilla <josigna.cp@MacBook-Pro-de-Jose.local>
2018-09-19 17:19:36 -0300

    Se crea metodo para ingresar usuarios de prueba a traves de SeedUsers

--- IdaraEncuestas-API/Startup.cs
+++ IdaraEncuestas-API/Startup.cs
@@ -37,6 +37,7 @@ namespace IdaraEncuestas_API
             services.AddDbContext<DataContext>(x => x.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
             services.AddCors();
+            services.AddTransient<Seed>();
             services.AddScoped<IAuthRepository, AuthRepository>();
             services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(Options => {
                 Options.TokenValidationParameters = new TokenValidationParameters{
@@ -50,7 +51,7 @@ namespace IdaraEncuestas_API
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, Seed seeder)
         {
             if (env.IsDevelopment())
             {
@@ -73,7 +74,8 @@ namespace IdaraEncuestas_API
                 //app.UseHsts();
             }
 
-            //app.UseHttpsRedirection();
+            // app.UseHttpsRedirection();
+            // seeder.SeedUsers();
             app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
             app.UseAuthentication();
             app.UseMvc();
